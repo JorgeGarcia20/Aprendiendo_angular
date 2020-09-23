f566ede26df57592f6eddbf6d982cc2a5ead5619
JorgeGarcia20 <alberto-garcia20@outlook.com>
2020-09-22 23:03:15 -0500

    formularios

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,7 +8,8 @@ import { ShoesComponent } from './shoes/shoes.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { Route } from '@angular/compiler/src/core';
-import { ExternoComponent } from './externo/externo.component'
+import { ExternoComponent } from './externo/externo.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 //Array de rutas
 const appRoutes: Routes = [
@@ -18,6 +19,7 @@ const appRoutes: Routes = [
   { path: 'cursos', component: CursosComponent },
   { path: 'cursos/:nombre/:followers', component: CursosComponent },
   { path: 'externo', component: ExternoComponent },
+  { path: 'contacto', component: ContactoComponent },
   { path: '**', component: HomeComponent }
 ];
 
